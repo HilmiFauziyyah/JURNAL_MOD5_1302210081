51698d92c012c51c1701af1cdfe44b6957e589b9
hilmifauziyyah <hilmifs2113@gmial.com>
2023-03-24 07:47:36 +0700

    HilmiFauziyyah

---
+++ MODUL5_1302210081/MODUL5_1302210081/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MODUL5_1302210081
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
